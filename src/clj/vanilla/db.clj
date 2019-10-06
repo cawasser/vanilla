080983f4e0fb5f776bc007bb71d8bc22a3e7ff15
Chris Wasser <cawasser@mac.com>
2019-10-06 15:36:09 -0400

    data provider for pie-cart kind of data (:usage-data)

--- src/clj/vanilla/db.clj
+++ src/clj/vanilla/db.clj
@@ -2,19 +2,30 @@
 
 
 (defn spectrum-data []
-  [{:name "trace-1"
+  [{:name   "trace-1"
     :values (into []
                   (take 200
                         (repeatedly #(+ 5.0
                                         (rand 5)))))}
-   {:name "trace-2"
+   {:name   "trace-2"
     :values (into []
                   (take 200
                         (repeatedly #(+ 5.0
                                         (rand 5)))))}
-   {:name "trace-3"
+   {:name   "trace-3"
     :values (into []
                   (take 200
                         (repeatedly #(+ 5.0
                                         (rand 5)))))}])
+
+
+(defn usage-data []
+  [["Apples" (rand 100)]
+   ["Pears" (rand 100)]
+   ["Oranges" (rand 100)]
+   ["Plums" (rand 100)]
+   ["Bananas" (rand 100)]
+   ["Peaches" (rand 100)]
+   ["Prunes" (rand 100)]
+   ["Avocados" (rand 100)]])
 
