e372d905f60cbbdb1b45aac4b4f81020c111ae16
Chris Wasser <cawasser@mac.com>
2019-10-13 22:16:05 -0400

    got the clock text centered H & V correctly now

--- src/cljs/vanilla/widgets/simple_text.cljs
+++ src/cljs/vanilla/widgets/simple_text.cljs
@@ -1,20 +1,23 @@
 (ns vanilla.widgets.simple-text
-    (:require [reagent.core :as r :refer [atom]]
-              [dashboard-clj.widgets.core :as widget-common]
-              [vanilla.widgets.basic-widget :as basic]))
+  (:require [reagent.core :as r :refer [atom]]
+            [dashboard-clj.widgets.core :as widget-common]
+            [vanilla.widgets.basic-widget :as basic]))
 
 
 (widget-common/register-widget
- :simple-text
- (fn [data options]
-   ;(.log js/console ":simple-text" (str data) (str options))
+  :simple-text
+  (fn [data options]
+    ;(.log js/console ":simple-text" (str data) (str options))
 
-   [basic/basic-widget data options
+    [basic/basic-widget data options
 
-    [:div {:style {:width "100%"
-                   :display :flex :align-items :center}}    ;(get-in options [:viz :style] {})}
-     [:p {:style {:fontSize    "50px"
-                  :font-weight "bold"
-                  :color       "blue"}}
+     [:div {:style {:width "100%"
+                    :text-align :center
+                    :border-style  (get-in options [:viz :debug] :none)}}
+      [:p {:style {:fontSize    "50px"
+                   :font-weight "bold"
+                   :color       "blue"}}
+                                    ;
 
-      (get-in data [:data :text])]]]))
+
+       (get-in data [:data :text])]]]))
