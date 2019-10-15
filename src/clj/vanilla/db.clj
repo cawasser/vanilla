ab1d6e22df2dc70df2c159d9af5339532c6bf997
George Pierce <gpierce3@hotmail.com>
2019-10-15 13:27:16 -0400

    Added code for back end entities 3 and 4.

--- src/clj/vanilla/db.clj
+++ src/clj/vanilla/db.clj
@@ -29,3 +29,38 @@
    ["Prunes" (rand 100)]
    ["Avocados" (rand 100)]])
 
+;
+; This function returns a data structure that shows the percentage free/used of a resource pool.
+; The structure is returned as a map.
+; The first element shows how much of the pool is used.
+; The second element shows how much of the pool is available.
+;
+(defn entity3-data-total []
+  {:used 43.3 :available 56.7})
+
+;
+; This function returns a data structure that shows how much of various resources are consumed by various consumers.
+; The structure is returned as a vector of maps.
+; The first element names the consumer.
+; The rest of the key-value pairs describe the resource and the amount consumed.
+;
+(defn entity3-data-by-consumer []
+  [{:consumer "Consumer 1" :resource1 15 :resource2 0  :resource3 0},
+   {:consumer "Consumer 2" :resource1 90 :resource2 30 :resource3 0},
+   {:consumer "Consumer 3" :resource1 30 :resource2 20 :resource3 75}])
+
+;
+; This function returns a data structure that the yearly capacity of certain resources.
+; The structure is a vector of maps
+; The map consists of two elements:
+;   Firstly the name of a resource
+;   Secondly, a vector of maps containing yearly capacity figures
+;     Each of those maps contains an element for the year of interest
+;     and a second element with the capacity for that year.
+;
+(defn entity4-data []
+  [{:name "Requirements"
+    :yearly-capacity [{:year 2014 :capacity 17} {:year 2015 :capacity 19.5} {:year 2016 :capacity 21.5}]},
+   {:name "Resource1"
+    :yearly-capacity [{:year 2014 :capacity 12.81} {:year 2015 :capacity 14.63} {:year 2016 :capacity 17.42}]}])
+
--- src/clj/vanilla/fetcher.clj
+++ src/clj/vanilla/fetcher.clj
@@ -23,3 +23,22 @@
   {:title "Time"
    :text  (.format (java.time.LocalDateTime/now)
                    (java.time.format.DateTimeFormatter/ofPattern "hh:mm:ss"))})
+
+;
+; Use this function to create a Combo Chart
+; Note that it is pulling info from two places
+;
+(defn fetch-entity3 []
+  (prn "fetching entity3")
+
+  {:title "Resource Consumption"
+   :entity3-data (merge (db/entity3-data-total) db/entity3-data-by-consumer)})
+
+;
+; Use this function to create an Area Chart
+;
+(defn fetch-entity4 []
+  (prn "fetching entity4")
+
+  {:title "Resource Capacity"
+   :entity4-data (db/entity4-data)})
