61e279b021fb05f98c5e7a4ab05839fc64de483a
Chris Wasser <cawasser@mac.com>
2019-11-03 14:54:55 -0500

    added notice block to events.cljs as to the use of "obsolete" re-frame

--- src/cljs/vanilla/events.cljs
+++ src/cljs/vanilla/events.cljs
@@ -3,6 +3,27 @@
   (:require [re-frame.core :as rf]))
 
 
+;;;;;;;;;;;;;;;;;;;;;;;
+;
+; NOTE!
+;
+; this is a fairly old version of re-frame (0.7.0), driven by
+; the dashboard-clj code, which itself, pretty old. We are
+; continuing to use this version to simplify the functional
+; upgrades we plan to make to dashboard-clj. Adding more re-frame
+; is currently a lower priority.
+;
+; Unfortunately, in the meantime, if you look at the current re-frame docs
+; you will find code that DOES NOT WORK in 0.7.0. re-frame has undergone
+; significant changes between 0.7.0 and 0.10.x (as of 2019-11-03). Be Aware!
+;
+; When we finally upgrade re-frame, this notice should be removed.
+;
+;;;;;;;;;;;;;;;;;;;;;;;
+
+
+
+
 
 ; subscriptions
 
