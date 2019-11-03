13e773464ce3fc7f62020447eba4ce66d796b859
Chris Wasser <cawasser@mac.com>
2019-11-03 15:26:46 -0500

    tweaked layout to pack the widgets in preparation for merging with the windrose-widget branch

--- src/cljs/vanilla/widget_layout.cljs
+++ src/cljs/vanilla/widget_layout.cljs
@@ -25,8 +25,8 @@
                                            :md {:x 0 :y 14 :w 4 :h 2}
                                            :sm {:x 0 :y 14 :w 2 :h 2 :static true}}}}
    :spectrum-area-widget      {:layout-opts
-                               {:position {:lg {:x 0 :y 16 :w 4 :h 2}
-                                           :md {:x 0 :y 16 :w 4 :h 2}
+                               {:position {:lg {:x 0 :y 16 :w 2 :h 2}
+                                           :md {:x 0 :y 16 :w 2 :h 2}
                                            :sm {:x 0 :y 16 :w 2 :h 2 :static true}}}}
    :map-widget                {:layout-opts
                                {:position {:lg {:x 4 :y 5 :w 2 :h 3}
@@ -47,9 +47,9 @@
                                            :md {:x 4 :y 3 :w 2 :h 3}
                                            :sm {:x 0 :y 3 :w 2 :h 3 :static true}}}}
    :bubble-widget             {:layout-opts
-                               {:position {:lg {:x 4 :y 14 :w 2 :h 3}
-                                           :md {:x 4 :y 14 :w 2 :h 3}
-                                           :sm {:x 0 :y 14 :w 2 :h 3 :static true}}}}
+                               {:position {:lg {:x 4 :y 14 :w 2 :h 2}
+                                           :md {:x 4 :y 14 :w 2 :h 2}
+                                           :sm {:x 0 :y 14 :w 2 :h 2 :static true}}}}
    :spectrum-line-widget      {:layout-opts
                                {:position {:lg {:x 4 :y 9 :w 2 :h 2}
                                            :md {:x 4 :y 9 :w 2 :h 2}
@@ -57,8 +57,8 @@
    :dependency-widget         {:layout-opts
                                {:position {:lg {:x 3 :y 11 :w 3 :h 3}
                                            :md {:x 3 :y 11 :w 3 :h 3}
-                                           :sm {:x 3 :y 11 :w 3 :h 3 :static true}}}}
+                                           :sm {:x 0 :y 11 :w 3 :h 3 :static true}}}}
    :spectrum-column-widget    {:layout-opts
-                               {:position {:lg {:x 4 :y 16 :w 2 :h 2}
-                                           :md {:x 4 :y 16 :w 2 :h 2}
+                               {:position {:lg {:x 2 :y 16 :w 2 :h 2}
+                                           :md {:x 2 :y 16 :w 2 :h 2}
                                            :sm {:x 0 :y 16 :w 2 :h 2 :static true}}}}})
