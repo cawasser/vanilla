86dda3a5fd61be82d1c467ac64f8cfb2d3c633bc
austinpaine <austin.paine@ngc.com>
2020-01-20 09:39:49 -0800

    merged with master but onLayoutChange didnt seem to merge right? removed the messy if in there to match master

--- src/cljs/vanilla/grid.cljs
+++ src/cljs/vanilla/grid.cljs
@@ -59,26 +59,11 @@
 (defn onLayoutChange [on-change prev new]
   ;; note the need to convert the callbacks from js objects
 
+    (rf/dispatch [:update-layout (js->clj new :keywordize-keys true)])
 
-  (let [chg (js->clj new :keywordize-keys true)
-        fst (first chg)]
-    (if (and
-          (not (empty? chg))
-          (= 1 (count chg))
-          (not= (:i fst) "null"))
-      (do
-        (prn "onLayoutChange " on-change
-          " //// prev " prev
-          " //// new " chg
-          " //// empty? " (empty? chg)
-          " //// count " (count chg)
-          " //// first id " (:i fst))
+    (rf/dispatch [:save-layout (js->clj new :keywordize-keys true)])
 
-        (rf/dispatch [:update-layout (js->clj new :keywordize-keys true)])
-
-        (rf/dispatch [:save-layout (js->clj new :keywordize-keys true)])))
-
-    (on-change prev (js->clj new :keywordize-keys true))))
+    (on-change prev (js->clj new :keywordize-keys true)))
 
 
 
