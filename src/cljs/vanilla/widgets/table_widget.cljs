ecc9fd1b76023d550571f2663369bf34ad1eaed9
Chris Wasser <cawasser@mac.com>
2020-05-06 15:54:47 -0400

    - tweaking tables, table-widget, and the css for them

--- src/cljs/vanilla/widgets/table_widget.cljs
+++ src/cljs/vanilla/widgets/table_widget.cljs
@@ -10,27 +10,23 @@
   ;     " //// (options) " options)
 
   (let [header (get-in data [:data :meta-data])
-        body   (take 7 (get-in data [:data :series]))]
+        body   (get-in data [:data :series])]
 
     ;(prn "header " header
     ;     " //// body " body)
 
-    [:div.container
-     [:div.table-container
-      [:table.table.dark-mode.is-fullwidth
-       {:style {;:width          "100%"
-                ;:height         "100%"
-                :border-spacing "15px"
-                :table-layout   :auto}}
+    [:div.table-container
+     [:table.table.is-hoverable
+      [:thead
+       [:tr
+        (for [h header]
+          ^{:key (str (:name h))} [:th (str (:name h))])]]
 
-       [:thead
-        [:tr
-         (for [h header]
-           ^{:key (str (get h :name))} [:th {:style {:color "lightgray"}} (str (get h :name))])]]
-
-       [:tbody
-        (for [b body]
-          [:tr
-           (for [[k v] b]
-             (do
-               ^{:key (str v)} [:td (str v)]))])]]]]))
+      [:tbody
+       (doall
+         (for [b body]
+           ^{:key (str b)}
+           [:tr
+            (for [[k v] b]
+              (do
+                [:td (str v)]))]))]]]))
