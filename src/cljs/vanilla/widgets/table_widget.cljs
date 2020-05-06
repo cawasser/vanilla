35481f69bd0f83ae408e0c96f449978afb38e9b3
Chris Wasser <cawasser@mac.com>
2020-05-06 10:06:49 -0400

    - found some "dark-mode" themes for Bulma (especially for <table>)

--- src/cljs/vanilla/widgets/table_widget.cljs
+++ src/cljs/vanilla/widgets/table_widget.cljs
@@ -9,25 +9,28 @@
   ;     " //// data " data
   ;     " //// (options) " options)
 
-  ( let [header (get-in data [:data :meta-data])
-         body (get-in data [:data :series])]
+  (let [header (get-in data [:data :meta-data])
+        body   (take 7 (get-in data [:data :series]))]
 
     ;(prn "header " header
     ;     " //// body " body)
 
     [:div.container
-      [:table-container
-       [:table.is-hoverable {:style {:width          "100%"
-                                     :border-spacing "15px"
-                                     :table-layout   :fixed}}
-        [:thead
-         [:tr
-          (for [h header]
-            ^{:key (str (get h :name))} [:th (str (get h :name))])]]
+     [:div.table-container
+      [:table.table.dark-mode.is-fullwidth
+       {:style {;:width          "100%"
+                ;:height         "100%"
+                :border-spacing "15px"
+                :table-layout   :auto}}
 
-        [:tbody
-         (for [b body]
-           [:tr
-             (for [[k v] b]
-               (do
-                 ^{:key (str v)}[:td (str v)]))])]]]]))
+       [:thead
+        [:tr
+         (for [h header]
+           ^{:key (str (get h :name))} [:th {:style {:color "lightgray"}} (str (get h :name))])]]
+
+       [:tbody
+        (for [b body]
+          [:tr
+           (for [[k v] b]
+             (do
+               ^{:key (str v)} [:td (str v)]))])]]]]))
