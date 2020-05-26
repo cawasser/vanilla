43d126bd04a1d99e98bc897ed5716a037307ed74
George Pierce <gpierce3@hotmail.com>
2020-05-26 12:29:39 -0400

    created some common type-checking specs. changed validation to use them.

--- src/cljc/vanilla/specs/dataspecs.clj
+++ src/cljc/vanilla/specs/dataspecs.clj
@@ -4,10 +4,16 @@
             [clojure.tools.logging :as log]))
 
 ;
+; some common specs for 'type checking'
+;
+(s/def ::is-keyword keyword?)
+(s/def ::is-vector vector?)
+
+;
 ; specs for :data-format
 ; two 'simple' ones, and one composed of those two
 ;
-(s/def ::data-format-keyword keyword?)
+;(s/def ::data-format-keyword keyword?)
 (s/def ::data-format-in-set #{:data-format/carousel
                               :data-format/cont-n
                               :data-format/date-yl-yh
@@ -24,11 +30,11 @@
                               :data-format/task-link
                               :data-format/x-y
                               :data-format/x-y-n})
-(s/def ::data-format-valid (s/and ::data-format-in-set ::data-format-keyword))
+(s/def ::data-format-valid (s/and ::data-format-in-set ::is-keyword))
 
 
-(log/info "keyword this should be true = " (s/valid? ::data-format-keyword :data-format/lat-lon-label))
-(log/info "keyword this should be false = " (s/valid? ::data-format-keyword 42))
+(log/info "keyword this should be true = " (s/valid? ::is-keyword :data-format/lat-lon-label))
+(log/info "keyword this should be false = " (s/valid? ::is-keyword 42))
 (log/info "in-set this should be true = " (s/valid? ::data-format-in-set :data-format/lat-lon-label))
 (log/info "in-set this should be false = " (s/valid? ::data-format-in-set :data-format/lat-lon))
 (log/info "::data-format-valid keyword this should be true = " (s/valid? ::data-format-valid :data-format/lat-lon-label))
@@ -36,10 +42,10 @@
 (log/info "::data-format-valid in-set this should be true = " (s/valid? ::data-format-valid :data-format/lat-lon-label))
 (log/info "::data-format-valid in-set this should be false = " (s/valid? ::data-format-valid :data-format/lat-lon))
 
-(log/info "explain failure: " (s/explain-str ::data-format-keyword 42))
+(log/info "explain failure: " (s/explain-str ::is-keyword 42))
 
 (log/info "generate a value: " (gen/generate (s/gen ::data-format-in-set)))
-(log/info "generate a value: " (gen/generate (s/gen ::data-format-keyword)))
+(log/info "generate a value: " (gen/generate (s/gen ::is-keyword)))
 
 ;
 ;this didn't work if ::data-format-valid had the keyword check first in the s/and!!!
