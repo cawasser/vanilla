125277ee9570c923b1c997218d1b8a30f791f3a5
Chris Wasser <cawasser@mac.com>
2020-05-27 14:46:33 -0400

    - switched to pull-syntax for several data-sourses

--- src/clj/vanilla/beam_location_service.clj
+++ src/clj/vanilla/beam_location_service.clj
@@ -8,18 +8,14 @@
 
 
 (defn- get-data [band]
-  (->> (d/q '[:find ?name ?lat ?lon ?radius ?type
+  (->> (d/q '[:find [(pull ?e [*]) ...]
               :where [?e :band ?band]
               [?e :beam-id ?name]
-              [?e :lat ?lat]
-              [?e :lon ?lon]
-              [?e :radius ?radius]
-              [?e :type ?type]
               :in $ ?band]
          @excel/conn band)
-    (map (fn [[name lat lon radius t city]]
-           {:name (str band name) :lat lat :lon lon
-            :e {:diam (* radius 2) :purpose t}}))))
+    (map (fn [{:keys [band beam-id lat lon radius type]}]
+           {:name (str band beam-id) :lat lat :lon lon
+            :e {:diam (* radius 2) :purpose type}}))))
 
 
 (defn fetch-data [band]
@@ -35,6 +31,35 @@
   (sort-by :name (get-data "X"))
 
   (sort-by :name (get-data "Ka"))
+
+  (d/q '[:find ?name
+         :where [?e :beam-id ?name]]
+    @excel/conn)
+
+
+  (def db @excel/conn)
+  (def band "X")
+
+  (d/q '[:find [(d/pull ?e ?name) ...]
+         :where [?e :beam-id ?name]]
+    db)
+
+  (d/q '[:find ?e
+         :where [?e :beam-id ?name]]
+        db)
+
+  (def beams (d/q '[:find ?e
+                    :where [?e :beam-id ?name]]
+               db))
+
+  (d/pull db '[*] (ffirst beams))
+
+  (d/q '[:find [(pull ?e [*]) ...]
+         :where [?e :beam-id _]]
+    db)
+
+
+
 
   ())
 
--- src/clj/vanilla/terminal_list_service.clj
+++ src/clj/vanilla/terminal_list_service.clj
@@ -16,27 +16,10 @@
 
 (defn- get-data []
   (into []
-    (->> (d/q '[:find ?terminal-id ?lat ?lon
-                ?satellite-id ?tx-beam ?tx-channel ?rx-beam ?rx-channel
-                :where [?e :terminal-id ?terminal-id]
-                [?e :lat ?lat]
-                [?e :lon ?lon]
-                [?e :satellite-id ?satellite-id]
-                [?e :tx-beam ?tx-beam]
-                [?e :tx-channel ?tx-channel]
-                [?e :rx-beam ?rx-beam]
-                [?e :rx-channel ?rx-channel]]
+    (->> (d/q '[:find [(pull ?e [*]) ...]
+                :where [?e :terminal-id]]
            @excel/conn)
-      (sort-by first)
-      (map (fn [[t lat lon s txb txc rxb rxc]]
-             {:terminal-id  t
-              :lat          lat
-              :lon          lon
-              :satellite-id s
-              :tx-beam      txb
-              :tx-channel   txc
-              :rx-beam      rxb
-              :rx-channel   rxc})))))
+      (sort-by first))))
 
 
 (defn fetch-data []
@@ -48,3 +31,15 @@
    :series      (get-data)})
 
 
+(comment
+  (d/q '[:find [(pull ?e [*]) ...]
+         :where [?e :terminal-id]]
+    @excel/conn)
+
+
+  (->> (d/q '[:find [(pull ?e [*]) ...]
+              :where [?e :terminal-id]]
+        @excel/conn)
+    (sort-by first))
+
+  ())
