705a0db3df156b994b1f0700fddef8f06c8f4290
George Pierce <gpierce3@hotmail.com>
2020-06-08 10:43:13 -0400

    added some conform tests.

--- src/clj/vanilla/specs/latlonlabel.clj
+++ src/clj/vanilla/specs/latlonlabel.clj
@@ -7,7 +7,9 @@
 ; specs for the lat-lon-label data format
 ;
 (s/def ::name ::vsu/is-string)
-(s/def ::lat-lon-label (s/keys :req [::name ::vsc/lat ::vsc/lon]))
-(s/def ::lat-lon-label-un (s/keys :req-un [::name ::vsc/lat ::vsc/lon]))
+(s/def ::lat ::vsc/lat)
+(s/def ::lon ::vsc/lon)
+(s/def ::lat-lon-label (s/keys :req [::name ::lat ::lon]))
+(s/def ::lat-lon-label-un (s/keys :req-un [::name ::lat ::lon]))
 (s/def ::lat-lon-label-vector (s/coll-of ::lat-lon-label-un :kind vector?))
 
