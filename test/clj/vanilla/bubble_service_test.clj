1a0441c2be0d93f3bd8919921cacfa7db1556227
George Pierce <gpierce3@hotmail.com>
2020-06-22 07:56:47 -0400

    one more change for bubble service spec test. Removed some temporary tests.

--- test/clj/vanilla/bubble_service_test.clj
+++ test/clj/vanilla/bubble_service_test.clj
@@ -5,17 +5,6 @@
             [vanilla.bubble-service :as testee]
             [clojure.tools.logging :as log]))
 
-(def eltesto {:title "Bubble Data"
-              :data-format :data-format/x-y-n
-              :series  [{:name "Countries"
-                         :data [{:x 95 :y 95 :z 13.8 :name "BE" :country "Belgium"}]}]})
-(def eltesto1
-  {:name "Countries"
-   :data [{:x 95 :y 95 :z 13.8 :name "BE" :country "Belgium"}]})
-
-(def eltesto2
-  {:x 95 :y 95 :z 13.8 :name "BE" :country "Belgium"})
-
 
 (deftest fetch-data-test
   (log/info (s/explain-str ::spec/bubble-service (testee/fetch-data)))
