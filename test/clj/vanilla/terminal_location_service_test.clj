a95ee8d0e0e849f92cbc9cfcfd5a692f3b953c51
George Pierce <gpierce3@hotmail.com>
2020-06-24 10:35:01 -0400

    apparently forgot to add this.

--- test/clj/vanilla/terminal_location_service_test.clj
+++ test/clj/vanilla/terminal_location_service_test.clj
@@ -1,2 +1,15 @@
 (ns vanilla.terminal-location-service-test
-  (:require [clojure.test :refer :all]))
+  (:require [clojure.test :refer :all]
+            [clojure.spec-alpha2 :as s]
+            [vanilla.specs.terminal-location-service :as spec]
+            [vanilla.terminal-location-service :as testee]
+            [clojure.tools.logging :as log]))
+
+;(deftest fetch-data-test
+;  (log/info (testee/fetch-data))
+;  (is (= 1 1)))
+
+(deftest fetch-data-test
+  (log/info (s/explain-str ::spec/terminal-location-service (testee/fetch-data)))
+  (is (s/valid? ::spec/terminal-location-service (testee/fetch-data))))
+
